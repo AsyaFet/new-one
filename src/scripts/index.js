3a4137ffd8e3ea0efdb9747186b5dfe7767fca52
Asya Fetisova <aanastai.logos@gmail.com>
2020-04-23 14:30:57 +0300

    matrix 1part

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,82 @@
 import "@fortawesome/fontawesome-free/js/all";
 import "../styles/index.scss";
 import "bootstrap";
+
+// 0) Document loaded get list  an save to variable
+// 1) Add event Listener
+// 2) Find input field
+// 3) Get value
+// 4) Get UL
+// 5) Create new li element > (checkox, text, icon)
+
+let importantAndUrgent;
+let importantAndNotUrgent;
+let notImportantAndUrgent;
+let notImportantAndNotUrgent;
+
+(function () {
+  importantAndUrgent = document.getElementById("importantAndUrgent");
+  importantAndNotUrgent = document.getElementById("importantAndNotUrgent");
+  notImportantAndUrgent = document.getElementById("notImportantAndUrgent");
+  notImportantAndNotUrgent = document.getElementById(
+    "notImportantAndNotUrgent"
+  );
+  const quadrants = [
+    importantAndUrgent,
+    importantAndNotUrgent,
+    notImportantAndUrgent,
+    notImportantAndNotUrgent,
+  ];
+
+  quadrants.forEach((quadrant) => {
+    const button = quadrant.getElementsByTagName("button")[0];
+    button.addEventListener("click", () => {
+      addNewTask(quadrant);
+    });
+  });
+})();
+
+function addNewTask(quadrant) {
+  console.log(quadrant);
+  const value = getInputValue(quadrant);
+  if (value) {
+    addNewItem(value, quadrant);
+  } else {
+    alert("Please add value to input");
+  }
+}
+
+function getInputValue(quadrant) {
+  return quadrant.getElementsByClassName("new-task-input")[0].value;
+}
+
+function addNewItem(value, quadrant) {
+  const list = quadrant.getElementsByClassName("todoList")[0];
+  const newLi = getLiWithText(value);
+  list.appendChild(newLi);
+}
+
+function getLiWithText(value) {
+  const newListItem = document.createElement("li");
+  const checkbox = getCheckBox();
+  const text = document.createTextNode(" " + value);
+  const space = document.createTextNode(" ");
+  const icon = getDeleteIcon();
+  newListItem.appendChild(checkbox);
+  newListItem.appendChild(text);
+  newListItem.appendChild(space);
+  newListItem.appendChild(icon);
+  return newListItem;
+}
+
+function getCheckBox() {
+  const input = document.createElement("input");
+  input.type = "checkbox";
+  return input;
+}
+
+function getDeleteIcon() {
+  const i = document.createElement("i");
+  i.className = "fas fa-trash";
+  return i;
+}
