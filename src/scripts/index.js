7d674ea447f42777ed9284fb2c3ee3360eb87db4
Asya Fetisova <aanastai.logos@gmail.com>
2020-04-29 20:14:06 +0300

    if project

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,44 @@
 import "../styles/index.scss";
 
+let randomNumber;
 function init() {
-  const randomNumber = 5;
+  randomNumber = getRandomNumber(10);
+  console.log(`Random on init ${randomNumber}`);
   const input = document.getElementById("checkField");
   const checkBtn = document.getElementById("checkBtn");
 
   checkBtn.addEventListener("click", () => {
-    checkNumber(input);
+    checkNumber(input, randomNumber);
   });
 }
 
-function checkNumber(input) {
-  console.log(input.value);
+function checkNumber(input, number) {
+  if (input.value == "") {
+    confirm("Please add number");
+  } else {
+    checkValues(input, number);
+  }
 }
+
+function checkValues(input, number) {
+  const p = document.getElementById("status");
+  if (input.value > number) {
+    p.innerHTML = "Your number is greather";
+    p.className = "wrong";
+  } else if (input.value < number) {
+    p.innerHTML = "Your number less";
+    p.className = "wrong";
+  } else {
+    p.innerHTML = `You won! I was thinking about ${number}`;
+    p.className = "right";
+    input.value = "";
+    randomNumber = getRandomNumber(10);
+    console.log(`New random number ${randomNumber}`);
+  }
+}
+
+function getRandomNumber(n) {
+  return Math.floor(Math.random() * n + 1);
+}
+
+init();
