6a7b94d53c467070140f78d7915c33bd6b9418b6
Asya Fetisova <aanastai.logos@gmail.com>
2020-04-29 16:22:25 +0300

    func

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,93 +1,15 @@
-import "@fortawesome/fontawesome-free/js/all";
 import "../styles/index.scss";
-import "bootstrap";
 
-let importantAndUrgent;
-let importantAndNotUrgent;
-let notImportantAndUrgent;
-let notImportantAndNotUrgent;
+function init() {
+  const randomNumber = 5;
+  const input = document.getElementById("checkField");
+  const checkBtn = document.getElementById("checkBtn");
 
-(function () {
-  importantAndUrgent = document.getElementById("importantAndUrgent");
-  importantAndNotUrgent = document.getElementById("importantAndNotUrgent");
-  notImportantAndUrgent = document.getElementById("notImportantAndUrgent");
-  notImportantAndNotUrgent = document.getElementById(
-    "notImportantAndNotUrgent"
-  );
-  const quadrants = [
-    importantAndUrgent,
-    importantAndNotUrgent,
-    notImportantAndUrgent,
-    notImportantAndNotUrgent,
-  ];
-
-  quadrants.forEach((quadrant) => {
-    const button = quadrant.getElementsByTagName("button")[0];
-    button.addEventListener("click", () => {
-      addNewTask(quadrant);
-    });
+  checkBtn.addEventListener("click", () => {
+    checkNumber(input);
   });
-})();
-
-function addNewTask(quadrant) {
-  console.log(quadrant);
-  const value = getInputValue(quadrant);
-  if (value) {
-    addNewItem(value, quadrant);
-  } else {
-    alert("Please add value to input");
-  }
 }
 
-function getInputValue(quadrant) {
-  return quadrant.getElementsByClassName("new-task-input")[0].value;
-}
-
-function addNewItem(value, quadrant) {
-  const list = quadrant.getElementsByClassName("todoList")[0];
-  const newLi = getLiWithText(value, quadrant);
-  list.appendChild(newLi);
-}
-
-function moveIntoDone(li, quadrant) {
-  const secondList = quadrant.getElementsByClassName("doneList");
-  secondList[0].appendChild(li);
-}
-
-function getLiWithText(value, quadrant) {
-  const newListItem = document.createElement("li");
-
-  const checkbox = getCheckBox();
-  const text = document.createTextNode(" " + value);
-  const space = document.createTextNode(" ");
-  const icon = getDeleteIcon();
-
-  newListItem.appendChild(checkbox);
-  newListItem.appendChild(text);
-  newListItem.appendChild(space);
-  newListItem.appendChild(icon);
-
-  checkbox.addEventListener("click", () => {
-    moveIntoDone(newListItem, quadrant);
-  });
-
-  icon.addEventListener("click", () => {
-    newListItem.remove();
-  });
-
-  return newListItem;
-}
-
-function getCheckBox() {
-  const input = document.createElement("input");
-  input.type = "checkbox";
-  return input;
-}
-
-function getDeleteIcon() {
-  const i = document.createElement("i");
-  const span = document.createElement("span");
-  i.className = "fas fa-trash";
-  span.appendChild(i);
-  return span;
+function checkNumber(input) {
+  console.log(input.value);
 }
